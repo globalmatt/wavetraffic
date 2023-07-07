6314765ed6acdde899ce58a9edf46c059984ce15
Matt Doyle <matt@elated.com>
2023-07-07 17:55:10 +1000

    Implement info windows
    -   When the user clicks a marker, display the relevant incident info in
        a popup window.
    
    -   If the user clicks another marker, close the first popup before
        displaying the second.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import { useState, useCallback } from "react";
-import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+import { flushSync } from "react-dom";
+
+import {
+    GoogleMap,
+    useJsApiLoader,
+    Marker,
+    InfoWindow,
+} from "@react-google-maps/api";
 
 import "./App.css";
 
+// Load the JSON incident data.
 import incidentsJSON from "./data/incidents.json";
 const incidents = incidentsJSON.incidents;
 
@@ -25,6 +33,7 @@ function App() {
 
     const [map, setMap] = useState(null);
     const [visibleIncidents, setVisibleIncidents] = useState([]);
+    const [selectedIncident, setSelectedIncident] = useState(null);
 
     /**
      * When the map has loaded, set the bounds to fit all incidents.
@@ -51,6 +60,13 @@ function App() {
     }, []);
 
     /**
+     * When an incident's info window is closed, deselect the incident.
+     */
+    const handleInfoWindowCloseClick = () => {
+        setSelectedIncident(null);
+    };
+
+    /**
      * After the map is changed, update the list of visible incidents.
      */
     const updateList = () => {
@@ -68,10 +84,31 @@ function App() {
         );
     };
 
+    /**
+     * When a marker has loaded, associate it with the incident that
+     * it relates to, so that we can anchor the <InfoWindow> to the
+     * marker when displaying it.
+     *
+     * @param {google.maps.Marker} marker The loaded marker.
+     * @param {Object} incident The incident that the marker relates to.
+     */
+    const saveIncidentMarker = (marker, incident) => {
+        incident.marker = marker;
+    };
+
+    const handleMarkerClick = (e, incident) => {
+        // Force a re-render for this first state update
+        // to ensure any existing <InfoWindow> is unmounted.
+        flushSync(() => setSelectedIncident(null));
+
+        // Now display the new <InfoWindow>.
+        setSelectedIncident(incident);
+    };
+
     return isLoaded ? (
         <div className="app">
             <div className="incidentList">
-                <h1>Incidents</h1>
+                <h2>Incidents</h2>
                 <ul>
                     {visibleIncidents.map((incident) => (
                         <li key={incident.id}>
@@ -96,8 +133,24 @@ function App() {
                                 lat: parseFloat(incident.lat),
                                 lng: parseFloat(incident.long),
                             }}
-                        />
+                            onLoad={(marker) =>
+                                saveIncidentMarker(marker, incident)
+                            }
+                            onClick={(e) => handleMarkerClick(e, incident)}
+                        ></Marker>
                     ))}
+                    {selectedIncident && (
+                        <InfoWindow
+                            anchor={selectedIncident.marker}
+                            onCloseClick={handleInfoWindowCloseClick}
+                        >
+                            <>
+                                <h2>{selectedIncident.alert_type}</h2>
+                                <h3>{selectedIncident.title}</h3>
+                                <p>{selectedIncident.description}</p>
+                            </>
+                        </InfoWindow>
+                    )}
                 </>
             </GoogleMap>
         </div>
