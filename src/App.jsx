0a7a3b0d192793d623f464a5c35fc52d68d97c0a
Matt Doyle <matt@elated.com>
2023-07-07 13:18:34 +1000

    Get map working with incidents
    -   Import incidents JSON.
    
    -   Display markers on map.
    
    -   Set map bounds to encompass all markers.
    
    -   Display incident list filtered by currently-visible incidents.

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,106 @@
 import { useState, useCallback } from "react";
-
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
 
 import "./App.css";
+
+import incidentsJSON from "./data/incidents.json";
+const incidents = incidentsJSON.incidents;
 
 const containerStyle = {
     width: "400px",
     height: "400px",
 };
 
-const center = {
-    lat: -3.745,
-    lng: -38.523,
-};
-
+/**
+ * The main app component.
+ *
+ * @component App
+ * @returns ReactElement The app content.
+ */
 function App() {
     const { isLoaded } = useJsApiLoader({
         id: "google-map-script",
-        googleMapsApiKey: "",
+        googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
     });
 
     const [map, setMap] = useState(null);
+    const [visibleIncidents, setVisibleIncidents] = useState([]);
 
-    const onLoad = useCallback(function callback(map) {
-        // This is just an example of getting and using the map instance!!! don't just blindly copy!
-        const bounds = new window.google.maps.LatLngBounds(center);
+    /**
+     * When the map has loaded, set the bounds to fit all incidents.
+     */
+    const handleMapLoad = useCallback(function callback(map) {
+        const bounds = new window.google.maps.LatLngBounds();
+
+        incidents.forEach((incident) => {
+            bounds.extend({
+                lat: parseFloat(incident.lat),
+                lng: parseFloat(incident.long),
+            });
+        });
+
         map.fitBounds(bounds);
-
         setMap(map);
     }, []);
 
-    const onUnmount = useCallback(function callback(map) {
+    /**
+     * When the map has unmounted, remove its reference.
+     */
+    const handleMapUnmount = useCallback(function callback() {
         setMap(null);
     }, []);
 
+    /**
+     * After the map is changed, update the list of visible incidents.
+     */
+    const updateList = () => {
+        if (!map) return;
+
+        const bounds = map.getBounds();
+
+        setVisibleIncidents(
+            incidents.filter((incident) =>
+                bounds.contains({
+                    lat: parseFloat(incident.lat),
+                    lng: parseFloat(incident.long),
+                })
+            )
+        );
+    };
+
     return isLoaded ? (
-        <GoogleMap
-            mapContainerStyle={containerStyle}
-            center={center}
-            zoom={10}
-            onLoad={onLoad}
-            onUnmount={onUnmount}
-        >
-            {/* Child components, such as markers, info windows, etc. */}
-            <></>
-        </GoogleMap>
+        <div className="app">
+            <div className="incidentList">
+                <h1>Incidents</h1>
+                <ul>
+                    {visibleIncidents.map((incident) => (
+                        <li key={incident.id}>
+                            <h2>{incident.alert_type}</h2>
+                            <p>{incident.title}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                zoom={10}
+                onLoad={handleMapLoad}
+                onUnmount={handleMapUnmount}
+                onIdle={updateList}
+            >
+                <>
+                    {incidents.map((incident) => (
+                        <Marker
+                            key={incident.id}
+                            position={{
+                                lat: parseFloat(incident.lat),
+                                lng: parseFloat(incident.long),
+                            }}
+                        />
+                    ))}
+                </>
+            </GoogleMap>
+        </div>
     ) : (
         <></>
     );
